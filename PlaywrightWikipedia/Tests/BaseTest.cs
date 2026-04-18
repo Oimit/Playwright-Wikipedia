e8c382d96f0a4f6585c6eb7ae70adbe31e5ebdd2
Amit Cohen <Amit.Cohen@bagirasys.com>
2026-04-18 17:59:40 +0300

    Feat(HTML_Report): Adding HTML Report using ExtentReports

--- PlaywrightWikipedia/Tests/BaseTest.cs
+++ PlaywrightWikipedia/Tests/BaseTest.cs
@@ -1,10 +1,55 @@
 using Microsoft.Playwright.NUnit;
 using Microsoft.Playwright;
+using NUnit.Framework;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
 
 namespace PlaywrightWikipedia.Tests
 {
     public class BaseTest : PageTest
     {
+        private static ExtentReports _extent;
+        private static ExtentSparkReporter _htmlReporter;
+
+        protected ExtentTest _test;
+
+        [OneTimeSetUp]
+        public static void InitReport()
+        {
+            if (_extent != null) return; // Already initialized, don't overwrite
+            
+            var projectRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
+            var reportPath = Path.Combine(projectRoot, "Reports", "TestReport.html");
+            Directory.CreateDirectory(Path.GetDirectoryName(reportPath)!);
+
+            _htmlReporter = new ExtentSparkReporter(reportPath);
+            _extent = new ExtentReports();
+            _extent.AttachReporter(_htmlReporter);
+        }
+
+        [SetUp]
+        public void InitTest()
+        {
+            _test = _extent.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+
+        [TearDown]
+        public void LogResult()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            if (status == NUnit.Framework.Interfaces.TestStatus.Passed)
+                _test.Pass("Test passed.");
+            else if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+                _test.Fail(TestContext.CurrentContext.Result.Message);
+        }
+
+        [OneTimeTearDown]
+        public static void FlushReport()
+        {
+            _extent.Flush();
+        }
+
         public override BrowserNewContextOptions ContextOptions()
         {
             return new BrowserNewContextOptions
